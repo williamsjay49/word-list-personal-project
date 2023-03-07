28e4d141a45bbc62b3ff8500de50552b0c53dc82
johnson williams <williams.gb55@gmail.com>
2023-03-07 17:50:08 +0900

    fourth_commit

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 const formInput = document.querySelector('form input');
 const addBtn = document.querySelector('.display__clear');
+const formField = document.querySelector('form');
+listBtn = document.querySelector('ul');
 
-
+//Add Items
 function appendWord(e) {
     e.preventDefault();
 
     const item = formInput.value;
-    console.log(formInput.value);
-
-    const newWord = document.createElement('li');
-    newWord.className = 'mini-section__padding';
+    if (item === '') {
+        alert('word is not inputed!');
+    } else {
+        const newWord = document.createElement('li');
+        const createTitle = createTextElement(item);
+        const createTitlIcon = createIcon("fa-solid fa-trash-can");
+        const mainList = document.querySelector('ul');
     
-    const createTitle = createTextElement(item);
-    newWord.appendChild(createTitle);
-
-    const createTitlIcon = createIcon("fa-solid fa-trash-can");
-    newWord.appendChild(createTitlIcon);
-    
-    const mainList = document.querySelector('ul');
-    mainList.appendChild(newWord);
+        newWord.className = 'mini-section__padding';
+        newWord.appendChild(createTitle);
+        newWord.appendChild(createTitlIcon);
+        mainList.appendChild(newWord);
+    }
 
     formInput.value = '';
 }
 
 function createTextElement(arg) {
     const textElement = document.createElement('h4');
-    textElement.textContent = arg;
+    textElement.appendChild(document.createTextNode(arg));
     return textElement;
 }
 
@@ -34,8 +36,16 @@ function createIcon(classes) {
     icon.className = classes;
     return icon;
 }
+
+// Remove items
+function removeItem(e) {
+    e.target.parentElement.remove();
+}
+
+// Initialize elements
 function init() {
-    addBtn.addEventListener('click', appendWord);
+    formField.addEventListener('submit', appendWord);
+    listBtn.addEventListener('click', removeItem);
 }
 
 init();
