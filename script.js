a819a3b9f530a3c3c898b206c6ea146462260b6f
johnson williams <williams.gb55@gmail.com>
2023-03-11 11:28:00 +0900

    sixthCommit

--- script.js
+++ script.js
@@ -2,29 +2,56 @@ const formInput = document.querySelector('form input');
 const removeBtn = document.querySelector('.display__clear');
 const formField = document.querySelector('form');
 const listBtn = document.querySelector('ul');
-
+const mainDisplay = document.querySelector('.display');
 
 //Add Items
-function appendWord(e) {
+function onAddItemSubmit(e) {
     e.preventDefault();
-
+    
     const item = formInput.value;
     if (item === '') {
         alert('word is not inputed!');
-    } else {
-        const newWord = document.createElement('li');
-        const createTitle = createTextElement(item);
-        const createTitlIcon = createIcon("fa-solid fa-trash-can");
-        const mainList = document.querySelector('ul');
-    
-        newWord.className = 'mini-section__padding';
-        newWord.appendChild(createTitle);
-        newWord.appendChild(createTitlIcon);
-        mainList.appendChild(newWord);
+        return;
     }
 
+    // call the add element to dom function
+    addItemToDOM(item);
+
+    //call the addItemToLocalStorage function
+    addItemToLocalStorage(item);
+
+    //check and clear display
+    checkUi();
+
+    //initialize the input field to empty
     formInput.value = '';
 }
+
+function addItemToDOM(item) {
+    const newWord = document.createElement('li');
+    const createTitle = createTextElement(item);
+    const createTitlIcon = createIcon("fa-solid fa-trash-can");
+
+    newWord.className = 'mini-section__padding';
+    newWord.appendChild(createTitle);
+    newWord.appendChild(createTitlIcon);
+    listBtn.appendChild(newWord);
+}
+
+function addItemToLocalStorage(item) {
+    let itemsFromStorage;
+    //check if there is any element in the array
+    if (localStorage.getItem("items") === null) {
+        itemsFromStorage = [];
+    } else {
+        itemsFromStorage = (JSON.parse(localStorage.getItem("items")));
+    }
+    //add new item into storage
+    itemsFromStorage.push(item);
+
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+}
+
 
 function createTextElement(arg) {
     const textElement = document.createElement('h4');
@@ -43,6 +70,8 @@ function removeItem(e) {
     if (e.target.classList.contains('fa-trash-can')) {
         e.target.parentElement.remove();
     }
+
+    checkUi();
 }
 
 // Clear All
@@ -50,23 +79,41 @@ function clearAll () {
     while (listBtn.firstChild) {
         listBtn.removeChild(listBtn.firstChild);            
     }
+
+    checkUi();
 }
 
+// check ui
 
+function checkUi() {
+    const currentList = document.querySelectorAll('li');
+    if (currentList.length === 0) {
+        mainDisplay.style.display = "none";
+        removeBtn.style.display = "none";
+    } else {
+        mainDisplay.style.display = "flex";
+        removeBtn.style.display = "block";
+    }
+
+}
 
 // Update item in the list
-function updateItem(e) {
-    e.target.
+function updateItem() {
+    const currentList = document.querySelectorAll('li');
+    currentList.forEach(e => {
+        console.log(e.target.innerHTML);
+    })
 }
 
 
 
 // Initialize elements
 function init() {
-    formField.addEventListener('submit', appendWord);
+    formField.addEventListener('submit', onAddItemSubmit);
     listBtn.addEventListener('click', removeItem);
-    listBtn.addEventListener('click', updateItem);
+    // listBtn.addEventListener('click', updateItem);
     removeBtn.addEventListener('click', clearAll);
+    checkUi();
 }
 
 init();
